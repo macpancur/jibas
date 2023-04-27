56826ff405847690a2cdf345048a6274115d8c5a
Qomaruddin Djamal <macpancur@gmail.com>
2023-04-27 21:21:37 +0700

    Create session.setting.php

---
+++ anjangsana/include/session.setting.php
@@ -0,0 +1,12 @@
+<?
+$koneksi = mysqli_connect ("localhost:3434","root","kebersamaan","jbsakad");
+/* Code Nama JIBAS Main Office */
+$THEME_NAME='JIBAS Anjangsana';
+$THEME_VERSION="1.0";
+/* Code Nama JIBAS Main Office */
+$DEV_NAME='Qomaruddin Djamal';
+$CODE_SUMBER='citramedia';
+/* Hak Cipta SISFO JIBAS */
+$YAYASAN='Yaysan Islam Hasan Kafrawi';
+
+?>
