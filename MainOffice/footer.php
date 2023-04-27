37f9f7c37b9a4c3eae5ba7e6605cebad0feb6271
Qomaruddin Djamal <macpancur@gmail.com>
2023-04-27 21:19:20 +0700

    footer.php

---
+++ MainOffice/footer.php
@@ -0,0 +1,5 @@
+    <strong>Copyright &copy; 2023 | <?=$G_YAYASAN ?> </strong> Themes <?=$THEME_NAME;?> by 
+    <?=$DEV_NAME ?>
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> <?=$THEME_VERSION ?>
+    </div>
